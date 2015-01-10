471d002ea685fe3bcbdd9278bb7ff9a0c95e021d
Raphael Amorim <rapha850@gmail.com>
2015-01-09 22:09:01 -0200

    refactoring code (particles)

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -20,34 +20,47 @@ var canvas,
 	particles = [],
 	colors = ['#f0fd36', '#f49ff1', '#f53eac', '#76fbfa'];
 
-var getRandColor = function() {
-	return Math.floor(Math.random() * colors.length);
+var getRand = function(type) {
+	if (type === 'size')
+		return (Math.floor(Math.random() * 8) * 10)
+
+	if (type === 'color')
+		return Math.floor(Math.random() * colors.length)
+
+	if (type === 'pos')
+		return [
+			(Math.floor(Math.random() * 200) * 10),
+			(Math.floor(Math.random() * 80) * 10)
+		]
+
+	return false
 };
 
-var getRand = function() {
-	return (Math.floor(Math.random() * 8) * 10);
-};
-
-var getRandPos = function() {
-	return [(Math.floor(Math.random() * 200) * 10), (Math.floor(Math.random() * 80) * 10)];
-};
+var drawParticle = function(x, y, size, color, opacity){
+	ctx.beginPath();
+	ctx.globalAlpha = opacity;
+	ctx.arc(x, y, size, 0, 2 * Math.PI);
+	ctx.fillStyle = color;
+	ctx.fill();
+	ctx.strokeStyle = color;
+	ctx.stroke();
+}
 
 function clean() {
 	ctx.clearRect(0, 0, canvas_width, canvas_height);
 
 	particles.forEach(function(p) {
-		// p[0] = x, p[1] = y, p[2] = color
-		// p[3] = globalAlpha, p[4] = size
+		/*
+			p[0] = x,
+			p[1] = y,
+			p[2] = color
+		 	p[3] = globalAlpha,
+		 	p[4] = size
+		*/
 
 		p[3] = p[3] - 0.06;
 
-		ctx.beginPath();
-		ctx.globalAlpha = p[3];
-		ctx.arc(p[0], p[1], p[4], 0, 2 * Math.PI);
-		ctx.fillStyle = p[2];
-		ctx.fill();
-		ctx.strokeStyle = p[2];
-		ctx.stroke();
+		drawParticle(p[0], p[1], p[4], p[2], p[3])
 
 		if (p[p.length - 1] && p[3] <= 0.0) {
 			ctx.clearRect(0, 0, canvas_width, canvas_height);
@@ -60,21 +73,16 @@ function clean() {
 }
 
 function update(args) {
-	var randColor = colors[getRandColor()],
-		randPos = getRandPos(),
-		randSize = getRand();
+	var color = colors[getRand('color')],
+		pos = getRand('pos'),
+		size = getRand('size'),
+		opacity = 1;
 
-	ctx.beginPath();
-	ctx.globalAlpha = 1;
-	ctx.arc(randPos[0], randPos[1], randSize, 0, 2 * Math.PI);
-	ctx.fillStyle = randColor;
-	ctx.fill();
-	ctx.strokeStyle = randColor;
-	ctx.stroke();
+	drawParticle(pos[0], pos[1], size, color, opacity)
 
 	times++;
 
-	particles.push([randPos[0], randPos[1], randColor, 1, randSize]);
+	particles.push([pos[0], pos[1], color, opacity, size]);
 
 	if (times >= limit) {
 		clearInterval(draw);
